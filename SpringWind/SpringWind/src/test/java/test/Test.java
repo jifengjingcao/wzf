e926d37044fe184a73c725cbc2406a93d92261db
wuzhifeng <839300274@qq.com>
2017-11-14 18:19:39 +0800

    提交

---
+++ SpringWind/SpringWind/src/test/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+
+import java.math.BigDecimal;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        BigDecimal zroe = new BigDecimal(0);
+
+        System.out.println(zroe.subtract(new BigDecimal(311.89)));
+    }
+}
